6feec6ef2a65145737090a7eca0312bd52b85a86
Jumanazar <jsr1611@outlook.com>
2022-04-04 13:01:32 +0900

    updated .idea

--- demo1/src/main/kotlin/com/example/demo1/Demo1Application.kt
+++ demo1/src/main/kotlin/com/example/demo1/Demo1Application.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate
 import java.util.*
 
 @EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 class Demo1Application {
 
--- demo1/src/main/kotlin/com/example/demo1/Services.kt
+++ demo1/src/main/kotlin/com/example/demo1/Services.kt
@@ -1,25 +1,12 @@
 package com.example.demo1
 
-import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.postForObject
+import org.springframework.cloud.openfeign.FeignClient
 
+@FeignClient
 interface Demo2Service {
 
     fun hello(): String
 
     fun message(id: Long) : MessageDemo2Dto
 
-}
-
-@Service
-class Demo2ServiceImpl(private val restTemplate: RestTemplate): Demo2Service {
-
-    override fun message(id: Long): MessageDemo2Dto {
-        return restTemplate.postForObject("http://demo2/message/${id}", null, MessageDemo2Dto::class.java)!!
-    }
-
-    override fun hello(): String {
-        return restTemplate.getForObject("http://demo2", String::class.java)!!
-    }
 }
